1cdc89ea97515b0c258429c60b6d2e01b9b06d9b
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2016-11-15 00:48:18 +0000

    Revert "Point Light"
    This reverts commit 7321381eb7321baeb3632cd233b33d476e10e593.

--- assets/shaders/parallaxMappingVS.glsl
+++ assets/shaders/parallaxMappingVS.glsl
@@ -9,25 +9,24 @@ layout(location=5) in vec3 vertexBinormals;
 
 out vec3 vertexNormalOut;
 out vec3 cameraDirectionOut;
+out vec3 lightDirectionOut;
 out vec2 texCoordsOut;
-out mat3 tangentMatrix;
 
 uniform mat4 MVP;
 uniform mat4 Model;
 
 uniform vec3 lightDirection;
-uniform vec3 cameraPos;
+uniform vec3 cameraPosition;
 
 void main()
 {
+	mat3 tangentMatrix = mat3(normalize(vertexNormal), normalize(vertexTangents), normalize(vertexBinormals));
+
 	vec3 vertexNormalModel = normalize(Model*vec4(vertexNormal, 0.0f)).xyz;
-	vec3 vertexTangentsModel=normalize(Model*vec4(vertexTangents, 0.0f)).xyz;
-	vec3 vertexBinormalsModel=normalize(Model*vec4(vertexBinormals, 0.0f)).xyz;
-	tangentMatrix=mat3(vertexTangentsModel,vertexBinormalsModel,vertexNormalModel);
-
 	vec3 worldPos = (Model*vec4(vertexPosition, 1.0)).xyz;
-	vec3 cameraDir = normalize(cameraPos - worldPos);
+	vec3 cameraDir = normalize(cameraPosition - worldPos);
 
+	lightDirectionOut = normalize(tangentMatrix * lightDirection);
 	cameraDirectionOut = normalize(tangentMatrix * cameraDir);
 	vertexNormalOut = normalize(tangentMatrix * vertexNormalModel);
 
